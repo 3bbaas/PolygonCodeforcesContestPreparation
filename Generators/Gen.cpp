64d869eafa6f08696b17d299bf4e425a6378da1e
Ahmed Abbas <ah.abbas333@gmail.com>
2024-01-29 07:58:14 +0200

    Gen something be ready but tutorial is not. Main Solution files are ready tests directory is ready also

---
+++ Generators/Gen.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+#define ln '\n'
+#define ll long long
+#define ld long double
+#define N ((int)2e5 + 5)
+#define oo ((ll)1e17 + 9)
+#define mod ((ll)1e9 + 7)
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define alr(arr) arr.rbegin(), arr.rend()
+#define kill(x) return void(cout << x << ln)
+#define init(ar, n) memset((ar), n, sizeof (ar))
+#define db(xx) cout << #xx << ": " << xx << ln
+#define fix(pre) cout << setprecision(pre) << fixed
+
+/* +:-:+:-:+:-:+:-:+:-:+:-:+:-[ Generator ]:-:+:-:+:-:+:-:+:-:+:-:+:-+:- */
+#define FILES 5
+//#define MultipleTestCase
+
+void s0lve() {
+
+
+
+
+    cout << '\n';
+}
+
+/* +:-:+:-:+:-:+:-:+:-:+:-:+:-[ Generator ]:-:+:-:+:-:+:-:+:-:+:-:+:-+:- */
+
+void main2() {
+    int tst = 1;
+#ifdef MultipleTestCase
+    tst = rnd.next(1, 100'00);
+    cout << tst << '\n';
+#endif
+    while (tst--) s0lve();
+}
+
+signed main(int argc, char **argv) {
+    registerGen(argc, argv, 1);
+    for (int i = 0; i < FILES; ++i) {
+        string FILE = R"(Generators\tests\testCase-)" + to_string(i) + ".txt";
+        freopen(FILE.c_str(), "w", stdout);
+        main2();
+    }
+    return 0;
+}
+
---
+++ Solution/MainSolution.cpp
@@ -0,0 +1,61 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ln '\n'
+#define ll long long
+#define pp pop_back()
+#define ps(x) pb((x))
+#define ld long double
+#define nl cout << '\n'
+#define _ev(x) !(x & 1)
+#define pb emplace_back
+#define clr(x) x.clear()
+#define N ((int)2e5 + 5)
+#define oo ((ll)1e17 + 9)
+#define mod ((ll)1e9 + 7)
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define sz(x) (int)(x).size()
+#define _pb(x) push_back((x))
+#define all(x) x.begin(), x.end()
+#define nod(x) floor(log10(x)) + 1
+#define alr(arr) arr.rbegin(), arr.rend()
+#define kill(x) return void(cout << x << ln)
+#define _m(ar, n) memset((ar),n,sizeof (ar))
+#define db(xx) cout << #xx << ": " << xx << ln
+#define _uni(v) v.erase(unique(all(v)), v.end())
+#define fix(pre) cout << setprecision(pre) << fixed
+
+void _3bbas_();
+
+/* +:-:+:-:+:-:+:-:+:-:+:-:+:-[ Solution ]:-:+:-:+:-:+:-:+:-:+:-:+:-+:- */
+
+void s0lve() {
+
+
+}
+
+/* +:-:+:-:+:-:+:-:+:-:+:-:+:-[ Solution ]:-:+:-:+:-:+:-:+:-:+:-:+:-+:- */
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int tst = 1;
+    cin >> tst;
+    while (tst--) s0lve();
+    return 0;
+}
+
+void _3bbas_() {
+#ifndef ONLINE_JUDGE
+    freopen(".in", "r", stdin);
+    freopen(".out", "w", stdout);
+#endif
+}
